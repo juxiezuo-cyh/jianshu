ee7689a985393d2e39aedd82a38b9b5147a247a4
chengyuhan <chengyuhan@xdfzx.com>
2020-12-08 21:19:55 +0800

    20201208--1

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,36 +1,63 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'; // 某一个部分
 
-class TodoList extends Component {
+// UI组件
+const TodoList = (props) => {
+  const { inputValue, list, deleteItem, changeInputValue, handleClick } = props;
 
-  render() {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <input onChange={this.props.changeInputValue} value={this.props.inputValue} type="text" />
-          <button>提交</button>
-        </div>
-        <ul>
-          <li>Dell</li>
-        </ul>
+        <input onChange={changeInputValue} value={inputValue} type="text" />
+        <button onClick={handleClick}>提交</button>
       </div>
-    )
-  }
-
+      <ul>
+        {
+          list.map((item, index) => {
+            return <li onClick={deleteItem.bind(this, index)} key={index}>{item}</li>
+          })
+        }
+      </ul>
+    </div>
+  )
 }
+// class TodoList extends Component {
 
+//   render() {
+
+//   }
+
+// }
+
+// 获取state里面的数据放到props里面
 const mapStateToProps = (state) => {
   return {
     inputValue: state.inputValue,
+    list: state.list
   }
 }
 
+// 定义action方法
 const mapDispatchToProps = (dispatch) => {
   return {
     changeInputValue(e) {
       const action = {
         type: 'change_input_value',
         value: e.target.value
+      }
+      dispatch(action);
+    },
+    handleClick() {
+      const action = {
+        type: 'add_item',
+      }
+      dispatch(action);
+    },
+    deleteItem(index, e) {
+      e.preventDefault();
+      const action = {
+        type: 'delete_item',
+        index
       }
       dispatch(action);
     }
